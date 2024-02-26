9f3f06df43a8bd1c093c27a08ab85c1c1ab0f38a
ghina <66651075+ghina958@users.noreply.github.com>
2024-02-26 21:45:59 +0300

    File Class Added

---
+++ MySpace/DomainModel/File.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MySpace.DomainModel
+{
+    public class File
+    {
+        public int Id { get; set; }
+       // [StringLength(maximumLength:)]
+        public string Url { get; set; }
+
+        public int NoteId { get; set; }
+        public Note Note { get; set; }
+    }
+}
